5e6aa870ced12f93b4ac7191296ef192ad325dfe
shaheer <shaheeraptech1@gmail.com>
2025-06-29 10:56:22 -0700

    first

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Hello world");
+
+
+
+let display = document.getElementById("inputBox");
+// console.log(display);
+
+
+let buttons = document.querySelectorAll("button");
+
+
+let buttonsArray = Array.from(buttons);
+
+let String = '';
+
+
+buttonsArray.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target.innerHTML);
+
+
+
+
+        if (e.target.innerHTML == "DEL") {
+            String = String.substring(0, String.length - 1);
+            display.value = String;
+
+        }
+        else if (e.target.innerHTML == "AC") {
+            String = " ";
+
+            display.value = String;
+        }
+        else if (e.target.innerHTML == "=") {
+            String = eval(String);
+            display.value = String;
+        }
+        else {
+            String += e.target.innerHTML;
+            display.value = String;
+        }
+
+
+
+
+
+
+
+    })
+})
+
